89de770a60c26a32e78de6bd24cd263e3b4a22b8
kushalhk26 <premakotresh2003@gmail.com>
2026-04-29 15:43:35 +0530

    Update GitHub repository URL and run app command

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 Write-Host "Downloading project..."
 
 if (!(Test-Path "pec-oec-auto-allocator")) {
-    git clone https://github.com/YOUR_USERNAME/pec-oec-auto-allocator.git
+    git clone https://github.com/kushalhk26/pec-oec-auto-allocator.git
 }
 
 cd pec-oec-auto-allocator
